cce9f295e01b488f3320ccc3d7c2db19a407cc5e
dev1 <c.rolland@protonmail.com>
2025-06-11 20:52:51 -0400

    feat: try omerxx version

--- README.md
+++ README.md
@@ -1,28 +1,6 @@
-# nix-dotfiles
+# Dotfiles symlinked on my machine
 
-## Getting Started From Scratch
-
-```shell
-sudo mkdir -p ~/.config/
-git clone https://github.com/ro11net/nix-dotfiles.git ~/.config/nix
-sudo chown $(id -nu):$(id -ng) ~/.config/nix
-cd ~/.config/nix
-
-# To use Nixpkgs unstable:
-nix flake init -t nix-darwin/master --extra-experimental-features "nix-command flakes"
-
-sed -i '' "s/Christians/MacBook-Pro/$(scutil --get LocalHostName)/" flake.nix
+### Install with stow:
+```bash
+stow .
 ```
-
-### Install the flake
-
-```shell
-sudo nix run nix-darwin --extra-experimental-features "nix-command flakes" -- switch --flake ~/.config/nix
-```
-
-### Update after modifying the **flake.nix**
-
-```shell
-sudo darwin-rebuild switch --flake ~/.config/nix
-```
-
---
+++ nix-darwin/flake.nix
@@ -0,0 +1,94 @@
+{
+  description = "My Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    # nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs;
+        [
+          vim
+          direnv
+          sshs
+          glow
+          nushell
+          carapace
+        ];
+      # services.nix-daemon.enable = true;
+      nix.settings.experimental-features = "nix-command flakes";
+      programs.zsh.enable = true;  # default shell on catalina
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+      system.stateVersion = 4;
+      nixpkgs.hostPlatform = "aarch64-darwin";
+      # security.pam.enableSudoTouchIdAuth = true;
+
+      users.users.crolland.home = "/Users/crolland";
+      home-manager.backupFileExtension = "backup";
+      # nix.configureBuildUsers = true;
+      # nix.useDaemon = true;
+      system.primaryUser = "crolland";
+      system.defaults = {
+        dock.autohide = true;
+        dock.mru-spaces = false;
+        finder.AppleShowAllExtensions = true;
+        finder.FXPreferredViewStyle = "clmv";
+        loginwindow.LoginwindowText = "devops-toolbox";
+        screencapture.location = "~/Pictures/screenshots";
+        screensaver.askForPasswordDelay = 10;
+      };
+
+      # Homebrew packages
+      homebrew = {
+        enable = true;
+        # brews = [
+        #   "mas"
+        # ];
+        casks = [
+        #   "hammerspoon"
+          "firefox"
+          "wireshark"
+          # "google-chrome"
+        #   "iina"
+        #   "the-unarchiver"
+        ];
+        brews = [
+          "imagemagick"
+        ];
+        # masApps = {
+        #   "Slack" = 803453959;
+        # };
+        onActivation.cleanup = "zap";
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
+      };
+    };
+  in
+  {
+    darwinConfigurations."MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      system = "aarch64-darwin";
+      modules = [
+	configuration
+        home-manager.darwinModules.home-manager {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.crolland = import ./home.nix;
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."MacBook-Pro".pkgs;
+  };
+}
