ad9be2f96d661528cd18f7df66c30c075a89e933
Christian Rolland <c.rolland@protonmail.com>
2025-06-11 22:43:24 -0400

    modify home files

--- nix-darwin.backup/home.nix
+++ nix-darwin.backup/home.nix
@@ -15,14 +15,14 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # ".zshrc".source = ~/dotfiles/zshrc/.zshrc;
+    ".zshrc".source = ./../zshrc/.zshrc;
     # ".config/wezterm".source = ~/dotfiles/wezterm;
     # ".config/skhd".source = ~/dotfiles/skhd;
     # ".config/starship".source = ~/dotfiles/starship;
     # ".config/zellij".source = ~/dotfiles/zellij;
-    # ".config/nvim".source = ~/dotfiles/nvim;
+    ".config/nvim".source = ./../nvim;
     # ".config/nix".source = ~/dotfiles/nix;
-    # ".config/nix-darwin".source = ~/dotfiles/nix-darwin;
+    ".config/nix-darwin".source = ./../nix-darwin;
     # ".config/tmux".source = ~/dotfiles/tmux;
     # ".config/ghostty".source = ~/dotfiles/ghostty;
     # ".config/aerospace".source = ~/dotfiles/aerospace;
